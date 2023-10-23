029836ecceb8da3df62d6d135a74805199f706da
Vinicch <vinicch.leme@gmail.com>
2023-10-23 13:56:15 -0300

    fix quickie

--- src/quick.rs
+++ src/quick.rs
@@ -2,22 +2,22 @@ pub fn quick_sort(arr: &mut [i32]) {
     qs(arr, 0, arr.len() - 1);
 }
 
-fn qs(arr: &mut [i32], lo: usize, hi: usize) {
-    if lo <= hi {
+fn qs(arr: &mut [i32], low: usize, high: usize) {
+    if low >= high {
         return;
     }
 
-    let pivot_index = partition(arr, lo, hi);
+    let pivot_index = partition(arr, low, high);
 
-    qs(arr, lo, pivot_index - 1);
-    qs(arr, pivot_index + 1, hi);
+    qs(arr, low, pivot_index - 1);
+    qs(arr, pivot_index + 1, high);
 }
 
-fn partition(arr: &mut [i32], lo: usize, hi: usize) -> usize {
-    let pivot = arr[hi];
-    let mut index = lo - 1;
+fn partition(arr: &mut [i32], low: usize, high: usize) -> usize {
+    let pivot = arr[high];
+    let mut index = low - 1;
 
-    for i in 0..hi {
+    for i in 0..high {
         if arr[i] <= pivot {
             index += 1;
             arr.swap(i, index);
@@ -25,7 +25,7 @@ fn partition(arr: &mut [i32], lo: usize, hi: usize) -> usize {
     }
 
     index += 1;
-    arr[hi] = arr[index];
+    arr[high] = arr[index];
     arr[index] = pivot;
 
     index
